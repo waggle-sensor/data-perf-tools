e64764d81fde31c1590fe4fbf463273ca0de6139
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-08-07 14:23:52 -0500

    added display

--- display.py
+++ display.py
@@ -4,6 +4,7 @@ import time
 from threading import Thread
 from queue import Queue
 
+
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 
@@ -11,30 +12,36 @@ from queue import Queue
 # URL = 'http://10.0.0.155/nph-h264.cgi'
 URL = 'http://10.0.0.155/nph-mjpeg.cgi'
 
-cap = cv2.VideoCapture(URL)
 
-# Check if camera opened successfully
-if cap.isOpened() is False:
-    print("Error opening video stream or file")
+def stream_images(q):
+    while True:
+        cap = cv2.VideoCapture(URL)
+        if cap.isOpened():
+            break
+        time.sleep(1)
 
-# Read until video is completed
-total = 0
-start = time.monotonic()
+    try:
+        while True:
+            ok, frame = cap.read()
+            if ok:
+                q.put(frame)
+    finally:
+        cap.release()
 
-while True:
-    ret, frame = cap.read()
-    if ret is False:
-        break
-    total += 1
-    print(total / (time.monotonic() - start), frame.shape)
-    # Display the resulting frame
-    cv2.imshow('Frame', frame)
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the video capture object
-cap.release()
+def main():
+    q = Queue()
+    t = Thread(target=stream_images, args=(q,), daemon=True)
+    t.start()
 
-# Closes all the frames
-cv2.destroyAllWindows()
+    try:
+        while True:
+            frame = q.get()
+            cv2.imshow('Frame', frame)
+            cv2.waitKey(1)
+    finally:
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
